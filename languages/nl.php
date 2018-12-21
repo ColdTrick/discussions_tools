86d247c4d817fec9decb3e8d920aee2e7efc9ae6
Jerome Bakker <jeabakker@coldtrick.com>
2018-12-21 14:05:17 +0100

    chore: updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,24 +1,26 @@
 <?php
 /**
- * This file was created by Translation Editor v5.1
- * On 2016-05-06 14:10
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-21 14:04
  */
 
 return array (
+  'widgets:start_discussion:name' => 'Start een discussie',
+  'widgets:start_discussion:description' => 'Start eenvoudig een discussie in een geselecteerde groep',
+  'widgets:discussion:name' => 'Laatste discussies',
+  'widgets:discussion:description' => 'Toon de laatste discussies',
+  'widgets:group_forum_topics:name' => 'Discussies',
+  'widgets:group_forum_topics:description' => 'Toon de laatste discussies',
   'forum:group_tool_option:description' => 'Laat groepsleden met elkaar discussiëren in een simpel forum.',
   'discussions_tools:discussion:confirm:open' => 'Weet je zeker dat je deze discussie wilt heropenen?',
   'discussions_tools:discussion:confirm:close' => 'Weet je zeker dat je deze discussie wilt sluiten?',
-  'discussions_tools:widgets:start_discussion:title' => 'Staat een discussie',
-  'discussions_tools:widgets:start_discussion:description' => 'Begin een discussie in de geselecteerde groep',
   'discussions_tools:widgets:start_discussion:login_required' => 'Om gebruik te kunnen maken van deze widget moet je zijn aangemeld',
   'discussions_tools:widgets:start_discussion:membership_required' => 'Je moet ten minste lid zijn van één groep om gebruik te kunnen maken van deze widget. Je kunt %shier%s interessante groepen vinden.',
   'discussions_tools:widgets:start_discussion:not_enabled' => 'Geen van je groepen heeft discussies ingeschakeld.',
   'discussions_tools:forms:discussion:quick_start:group' => 'Selecteer een groep om deze discussie in te plaatsen',
   'discussions_tools:forms:discussion:quick_start:group:required' => 'Selecteer een groep',
-  'discussions_tools:widgets:discussion:description' => 'Toont de laatste discussies',
   'discussions_tools:widgets:discussion:settings:group_only' => 'Toon alleen discussies van groepen waarvan je lid bent',
   'discussions_tools:widgets:discussion:more' => 'Toon meer discussies',
-  'discussions_tools:widgets:group_forum_topics:description' => 'Toon de laatste discussies',
   'discussions_tools:action:discussions:toggle_status:success:open' => 'De discussie is heropenend',
   'discussions_tools:action:discussions:toggle_status:success:close' => 'De discussie is gesloten',
 );
