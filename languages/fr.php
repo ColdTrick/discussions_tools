8effa017e029447cfb24369532c51a7194d30ad5
Dan d'Auge <76149200+danhype@users.noreply.github.com>
2021-02-10 18:43:20 +0100

    Create fr.php
    for v 4.0

---
+++ languages/fr.php
@@ -0,0 +1,22 @@
+<?php
+
+return array (
+  'forum:group_tool_option:description' => 'Permettre aux membres du groupe de lancer une discussion dans un format de forum simplifié.',
+  'discussions_tools:discussion:confirm:open' => 'Êtes-vous sûr de vouloir relancer ce sujet ?',
+  'discussions_tools:discussion:confirm:close' => 'Êtes-vous sûr de vouloir fermer ce sujet ?',
+  'widgets:start_discussion:name' => 'Commencer une discussion',
+  'widgets:start_discussion:description' => 'Lancer une discussion dans un groupe sélectionné',
+  'discussions_tools:widgets:start_discussion:login_required' => 'Pour pouvoir utiliser cette fonction, vous devez être connecté',
+  'discussions_tools:widgets:start_discussion:membership_required' => 'Vous devez être membre d\'au moins un groupe pour pouvoir utiliser cette fonction. Vous pouvez trouver des groupes %s ici %s.',
+  'discussions_tools:widgets:start_discussion:not_enabled' => 'Les discussions ne sont pas activées dans vos groupes.',
+  'discussions_tools:forms:discussion:quick_start:group' => 'Sélectionnez un groupe pour cette discussion',
+  'discussions_tools:forms:discussion:quick_start:group:required' => 'Veuillez choisir un groupe',
+  'widgets:discussion:name' => 'Dernières discussions',
+  'widgets:discussion:description' => 'Voir les dernières discussions',
+  'discussions_tools:widgets:discussion:settings:group_only' => 'Ne montrez que les discussions des groupes dont vous êtes membre',
+  'discussions_tools:widgets:discussion:more' => 'Voir plus de discussions',
+  'widgets:group_forum_topics:name' => 'Discussions',
+  'widgets:group_forum_topics:description' => 'Afficher les dernières discussions',
+  'discussions_tools:action:discussions:toggle_status:success:open' => 'Le sujet a été réouvert avec succès',
+  'discussions_tools:action:discussions:toggle_status:success:close' => 'Le sujet a été fermé avec succès',
+);
